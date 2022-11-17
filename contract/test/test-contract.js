adeeb80420c55231d4676950e0d172f1d275e677
Chris Boydstun <chrisboydstun@gmail.com>
2022-11-17 13:22:16 -0600

    add contract and test

--- contract/test/test-contract.js
+++ contract/test/test-contract.js
@@ -16,36 +16,121 @@ const dirname = path.dirname(filename);
 
 const contractPath = `${dirname}/../src/contract.js`;
 
-test('zoe - mint payments', async (t) => {
+test.before(async t => {
   const { zoeService } = makeZoeKit(makeFakeVatAdmin().admin);
-  const feePurse = E(zoeService).makeFeePurse();
-  const zoe = E(zoeService).bindDefaultFeePurse(feePurse);
 
-  // pack the contract
-  const bundle = await bundleSource(contractPath);
+  // #region bundle
+  const sell1Url = await importMetaResolve('./sell1.js', import.meta.url);
+  const sell1Path = url.fileURLToPath(sell1Url);
+  const sell1Bundle = await bundleSource(sell1Path);
+  // #endregion bundle
+  const installation = await E(zoeService).install(sell1Bundle);
 
-  // install the contract
-  const installation = E(zoe).install(bundle);
+  t.context = { zoeService, installation };
+});
 
-  const { creatorFacet, instance } = await E(zoe).startInstance(installation);
+test('sell 1 thing', async t => {
+  /** @type {{ zoeService: ZoeService }} */
+  const { zoeService: zoe, installation } = t.context;
 
-  // Alice makes an invitation for Bob that will give him 1000 tokens
-  const invitation = E(creatorFacet).makeInvitation();
+  // Both Alice and Bob know about the issuers and brands for moola, things
+  // but the mints are closely held.
+  const common = () => {
+    const { mint: moolaMint, ...moolaKit } = makeIssuerKit('Moola');
+    const { mint: thingMint, ...thingKit } = makeIssuerKit(
+      'Thing',
+      AssetKind.SET,
+    );
+    const moolaAmt = val => AmountMath.make(moolaKit.brand, val);
+    const thingAmt = val => AmountMath.make(thingKit.brand, val);
 
-  // Bob makes an offer using the invitation
-  const seat = E(zoe).offer(invitation);
+    const thing1 = thingMint.mintPayment(thingAmt(harden(['Thing 1'])));
+    const moola200 = moolaMint.mintPayment(moolaAmt(200n));
+    const share = makePromiseKit();
 
-  const paymentP = E(seat).getPayout('Token');
+    const scenario = harden({ item: thing1, payment: moola200, share });
 
-  // Let's get the tokenIssuer from the contract so we can evaluate
-  // what we get as our payout
-  const publicFacet = E(zoe).getPublicFacet(instance);
-  const tokenIssuer = E(publicFacet).getTokenIssuer();
-  const tokenBrand = await E(tokenIssuer).getBrand();
+    return { moolaKit, thingKit, moolaAmt, thingAmt, scenario };
+  };
+  const { moolaKit, thingKit, moolaAmt, thingAmt, scenario } = common();
 
-  const tokens1000 = AmountMath.make(tokenBrand, 1000n);
-  const tokenPayoutAmount = await E(tokenIssuer).getAmountOf(paymentP);
+  // Alice starts the contract and offers to sell thing1 for 100 moola.
+  const actAsAlice = async (thing1, shareInstance) => {
+    const thing1Amt = await E(thingKit.issuer).getAmountOf(thing1);
+    const proposalToSell = harden({
+      give: { Item: thing1Amt },
+      want: { Price: moolaAmt(100n) },
+    });
 
-  // Bob got 1000 tokens
-  t.deepEqual(tokenPayoutAmount, tokens1000);
+    const issuers = {
+      Item: thingKit.issuer,
+      Price: moolaKit.issuer,
+    };
+    const terms = { Item: thing1Amt, Price: moolaAmt(100n) };
+    const { instance, creatorFacet } = await E(zoe).startInstance(
+      installation,
+      issuers,
+      terms,
+    );
+
+    const invitationToSell = await E(creatorFacet).makeSellInvitation();
+    t.log('Alice offers to sell', proposalToSell);
+    const seat = await E(zoe).offer(
+      invitationToSell,
+      proposalToSell,
+      harden({ Item: thing1 }),
+    );
+    const result = await E(seat).getOfferResult();
+    t.log('result from offer to sell:', result);
+    shareInstance(instance); // with prospective buyers
+
+    const proceeds = await E(seat).getPayout('Price');
+    const procAmt = await E(moolaKit.issuer).getAmountOf(proceeds);
+    t.log('alice received:', procAmt);
+    t.deepEqual(procAmt, proposalToSell.want.Price);
+  };
+
+  // Bob buys thing1
+  const actAsBob = async (funds, instanceP) => {
+    const purse = moolaKit.issuer.makeEmptyPurse();
+    purse.deposit(funds);
+
+    // Bob checks the terms of the contract
+    const instance = await instanceP;
+    const { Price, Item } = await E(zoe).getTerms(instance);
+    t.deepEqual(Price, moolaAmt(100n));
+    t.deepEqual(Item, thingAmt(harden(['Thing 1'])));
+
+    const proposal = harden({ give: { Price }, want: { Item } });
+
+    // exercise: try this proposal instead
+    // const proposal = harden({ give: { Price: moolaAmt(50n) }, want: { Item } });
+
+    // or this one
+    // const proposal = harden({
+    //   give: { Price },
+    //   want: { Item: thingAmt(harden(['Thing 2'])) },
+    // });
+
+    t.log('bob proposes', proposal);
+
+    const publicFacet = E(zoe).getPublicFacet(instance);
+    const buyInv = await E(publicFacet).makeBuyInvitation();
+    const seat = await E(zoe).offer(buyInv, proposal, {
+      Price: purse.withdraw(proposal.give.Price),
+    });
+
+    const property = await E(seat).getPayout('Item');
+    const refund = await E(seat).getPayout('Price');
+    const pAmt = await E(thingKit.issuer).getAmountOf(property);
+    const rAmt = await E(moolaKit.issuer).getAmountOf(refund);
+    t.log('bob received', pAmt, rAmt);
+    t.deepEqual(pAmt, Item);
+    t.deepEqual(rAmt, moolaAmt(0n));
+  };
+
+  await Promise.all([
+    actAsAlice(scenario.item, scenario.share.resolve),
+    actAsBob(scenario.payment, scenario.share.promise),
+  ]);
 });
